a017776f1c0a285cbcea448d741dcb58b507cefb
iliailinskii <ilyarus34@gmail.com>
2024-02-12 14:16:54 +0100

    Initial Commit

---
+++ Safe data/Safe_dataApp.swift
@@ -0,0 +1,32 @@
+//
+//  Safe_dataApp.swift
+//  Safe data
+//
+//  Created by Ilia Ilinskii on 12/02/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Safe_dataApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
